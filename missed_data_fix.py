9bb912ffaad534426ee3a191675536d2474d574a
Mert Metin Erdemli <98667673+mertmetin1@users.noreply.github.com>
2024-03-07 18:08:02 +0300

    Update missed_data_fix.py

--- missed_data_fix.py
+++ missed_data_fix.py
@@ -12,13 +12,22 @@ print(data.dtypes)
 print()
 # data col formatting
 data['date'] = pd.to_datetime(data['date'], format='%m/%d/%Y')
+
 print("CHECK MİSSED DATA")
 print(data.isnull().sum())
 
-# replace null w 0
+# fill wtih min daily vacation number of country
+for country in data['country'].unique():
+    min_daily_vaccinations = data[data['country'] == country]['daily_vaccinations'].min()
+    data.loc[data['country'] == country, 'daily_vaccinations'] = data.loc[data['country'] == country, 'daily_vaccinations'].fillna(min_daily_vaccinations)
+
+# if missed data exist then replace null w 0
 data.fillna(0, inplace=True)
+print()
+print(data)
 
 # check
 print()
 print(data.isnull().sum())
-#done
+
+#OK
